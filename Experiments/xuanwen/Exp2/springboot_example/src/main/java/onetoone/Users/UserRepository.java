acf93c9dbe7ea85c6b9b3e654ee958dd94542138
xuanwen <xuanwen@iastate.edu>
2023-09-18 10:34:35 -0500

    Added tutorials

--- Experiments/xuanwen/Exp2/springboot_example/src/main/java/onetoone/Users/UserRepository.java
+++ Experiments/xuanwen/Exp2/springboot_example/src/main/java/onetoone/Users/UserRepository.java
@@ -10,11 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
  */ 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
     User findById(int id);
 
     @Transactional
     void deleteById(int id);
-
-    User findByLaptop_Id(int id);
 }

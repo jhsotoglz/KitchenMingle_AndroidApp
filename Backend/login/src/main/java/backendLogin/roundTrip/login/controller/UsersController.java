004ad1fb54c877cb1a065ef21dfeb78a60979adf
xuanwen <xuanwen@iastate.edu>
2023-10-03 18:01:14 -0500

    Added change and delete methods

--- Backend/login/src/main/java/backendLogin/roundTrip/login/controller/UsersController.java
+++ Backend/login/src/main/java/backendLogin/roundTrip/login/controller/UsersController.java
@@ -1,12 +1,14 @@
 package backendLogin.roundTrip.login.controller;
 
 
+import backendLogin.roundTrip.login.NotFoundException;
 import backendLogin.roundTrip.login.model.Users;
 import backendLogin.roundTrip.login.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsersController {
@@ -18,19 +20,34 @@ public class UsersController {
         return usersRepository.findAll();
     }
 
-    @PostMapping("users/post/{name}/{e}/{p}")
-    Users PostUsers(@PathVariable String name, @PathVariable String e, @PathVariable String p){
-        Users newUser = new Users();
-        newUser.setUsername(name);
-        newUser.setEmail(e);
-        newUser.setPassword(p);
+    @PostMapping("users/register")
+    Users AddUsers(@RequestBody Users newUser){
         usersRepository.save(newUser);
         return newUser;
     }
 
-    @PostMapping("users/post")
-    Users PostUsers(@RequestBody Users newUser){
-        usersRepository.save(newUser);
-        return newUser;
+    @PutMapping("users/put/{id}")
+    Users ChangeUsers(@PathVariable Long id, @RequestBody Users newInfo){
+        Optional<Users> existingUser = usersRepository.findById(id);
+        if(existingUser.isPresent()) {
+            Users updateUser = existingUser.get();
+            updateUser.setEmail(newInfo.getEmail());
+            updateUser.setPassword(newInfo.getPassword());
+            updateUser.setUsername(newInfo.getUsername());
+            usersRepository.save(updateUser);
+            return updateUser;
+        }else{
+            throw new NotFoundException("User with id "+ id + " not found");
+        }
+    }
+
+    @DeleteMapping("users/delete/{id}")
+    void DeleteUsers(@PathVariable Long id){
+        Optional<Users> existingUser = usersRepository.findById(id);
+        if(existingUser.isPresent()){
+            usersRepository.delete(existingUser.get());
+        }else{
+            throw new NotFoundException("User with id "+ id + " not found");
+        }
     }
 }

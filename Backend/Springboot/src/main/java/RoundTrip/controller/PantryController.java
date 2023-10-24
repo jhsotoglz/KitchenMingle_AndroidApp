73e726ae922e3c21b7a4d59bdee7c1c84189907c
xuanwen <xuanwen@iastate.edu>
2023-10-24 17:49:24 -0500

    Modify paths

--- Backend/Springboot/src/main/java/RoundTrip/controller/PantryController.java
+++ Backend/Springboot/src/main/java/RoundTrip/controller/PantryController.java
@@ -16,25 +16,25 @@ public class PantryController {
     @Autowired
     PantryRepository pantryRepository;
 
-    @GetMapping("/user/{userId}")
+    @GetMapping("/pantry/{userId}")
     public List<Pantry> getPantryForUser(@PathVariable Long userId) {
         // Retrieve pantry items for a specific user
         return pantryRepository.findByUser(new Users(userId));
     }
 
-    @PostMapping("/add")
+    @PostMapping("/pantry/add")
     public Pantry addItemToPantry(@RequestBody Pantry pantryItem) {
         // Save a new pantry item
         return pantryRepository.save(pantryItem);
     }
 
-    @DeleteMapping("/delete/{pantryItemId}")
+    @DeleteMapping("/pantry/delete/{pantryItemId}")
     public void deletePantryItem(@PathVariable Long pantryItemId) {
         // Delete a pantry item by ID
         pantryRepository.deleteById(pantryItemId);
     }
 
-    @PutMapping("/updateQuantity/{pantryItemId}")
+    @PutMapping("/pantry/updateQuantity/{pantryItemId}")
     public Pantry updatePantryItemQuantity(@PathVariable Long pantryItemId, @RequestParam int newQuantity) {
         // Find the pantry item by ID
         Optional<Pantry> pItem = pantryRepository.findById(pantryItemId);

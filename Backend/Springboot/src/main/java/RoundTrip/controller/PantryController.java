31cd2b6c3cc7700b83bd7675293be6cd1710bc0d
xuanwen <xuanwen@iastate.edu>
2023-10-24 18:02:32 -0500

    Implement increment & decrement for ingredient quantity

--- Backend/Springboot/src/main/java/RoundTrip/controller/PantryController.java
+++ Backend/Springboot/src/main/java/RoundTrip/controller/PantryController.java
@@ -16,25 +16,25 @@ public class PantryController {
     @Autowired
     PantryRepository pantryRepository;
 
-    @GetMapping("/pantry/{userId}")
+    @GetMapping("pantry/{userId}")
     public List<Pantry> getPantryForUser(@PathVariable Long userId) {
         // Retrieve pantry items for a specific user
         return pantryRepository.findByUser(new Users(userId));
     }
 
-    @PostMapping("/pantry/add")
+    @PostMapping("pantry/add")
     public Pantry addItemToPantry(@RequestBody Pantry pantryItem) {
         // Save a new pantry item
         return pantryRepository.save(pantryItem);
     }
 
-    @DeleteMapping("/pantry/delete/{pantryItemId}")
+    @DeleteMapping("pantry/delete/{pantryItemId}")
     public void deletePantryItem(@PathVariable Long pantryItemId) {
         // Delete a pantry item by ID
         pantryRepository.deleteById(pantryItemId);
     }
 
-    @PutMapping("/pantry/updateQuantity/{pantryItemId}")
+    @PutMapping("pantry/updateQuantity/{pantryItemId}")
     public Pantry updatePantryItemQuantity(@PathVariable Long pantryItemId, @RequestParam int newQuantity) {
         // Find the pantry item by ID
         Optional<Pantry> pItem = pantryRepository.findById(pantryItemId);
@@ -51,28 +51,41 @@ public class PantryController {
     }
 
     // Increment the quantity of an ingredient by 1
-//    @PostMapping("ingredient/increment/{id}")
-//    Ingredient incrementIngredientQuantity(@PathVariable Long id) {
-//        Ingredient ingredient = ingredientRepository.findById(id).orElse(null);
-//        if (ingredient != null) {
-//            ingredient.setQuantity(ingredient.getQuantity() + 1);
-//            ingredientRepository.save(ingredient);
-//        }
-//        return ingredient;
-//    }
-//
-//    // Decrement the quantity of an ingredient by 1 and delete if quantity reaches 0
-//    @PostMapping("ingredient/decrement/{id}")
-//    Ingredient decrementIngredientQuantity(@PathVariable Long id) {
-//        Ingredient ingredient = ingredientRepository.findById(id).orElse(null);
-//        if (ingredient != null && ingredient.getQuantity() > 0) {
-//            ingredient.setQuantity(ingredient.getQuantity() - 1);
-//            ingredientRepository.save(ingredient);
-//        } else if (ingredient != null && ingredient.getQuantity() == 1) {
-//            // If quantity becomes 0, delete the ingredient
-//            ingredientRepository.delete(ingredient);
-//            return null; // Return null as the ingredient no longer exists
-//        }
-//        return ingredient;
-//    }
+    @PostMapping("pantry/increment/{pantryItemId}")
+    Pantry incrementIngredientQuantity(@PathVariable Long pantryItemId) {
+        Optional<Pantry> pItem = pantryRepository.findById(pantryItemId);
+        if(pItem.isPresent()){
+            // Update the quantity
+            Pantry pantryItem = pItem.get();
+            int incr = pantryItem.getQuantity() + 1;
+            pantryItem.setQuantity(incr);
+
+            // Save the updated pantry item
+            return pantryRepository.save(pItem.get());
+        }else{
+            throw new NotFoundException("Ingredient " + pantryItemId + " not found.");
+        }
+    }
+
+    // Decrement the quantity of an ingredient by 1 and delete if quantity reaches 0
+    @PostMapping("pantry/decrement/{pantryItemId}")
+    Pantry decrementIngredientQuantity(@PathVariable Long pantryItemId) {
+        Optional<Pantry> pItem = pantryRepository.findById(pantryItemId);
+        if(pItem.isPresent()){
+            Pantry pantryItem = pItem.get();
+            int quantity = pantryItem.getQuantity();
+            if (quantity <= 1){
+                pantryRepository.delete(pantryItem);
+                return null;
+            }else{
+                int decr = quantity - 1;
+                pantryItem.setQuantity(decr);
+                return pantryRepository.save(pItem.get());
+            }
+        }else{
+            throw new NotFoundException("Ingredient " + pantryItemId + " not found.");
+        }
+    }
+
+    // TODO: Search recipes based on ingredients in pantry
 }

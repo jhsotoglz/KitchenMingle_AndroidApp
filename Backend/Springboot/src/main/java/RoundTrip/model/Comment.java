2f864d9157ff4e3903f59eecc7ff11a813a44760
jhsoto <jhsoto@iastate.edu>
2023-10-29 19:57:45 -0500

    Created comments model, repository and added needed dependencies

---
+++ Backend/Springboot/src/main/java/RoundTrip/model/Comment.java
@@ -0,0 +1,65 @@
+package RoundTrip.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Lob
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "sent")
+    private Date sent = new Date();
+
+
+    public Comment() {};
+
+    public Comment(String userName, String content) {
+        this.userName = userName;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getSent() {
+        return sent;
+    }
+
+    public void setSent(Date sent) {
+        this.sent = sent;
+    }
+}

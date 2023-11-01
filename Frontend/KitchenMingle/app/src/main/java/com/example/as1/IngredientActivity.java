9e9a23bac3ee65065419118f330c6fe3fa964b7a
Kat Amundson <kea1@iastate.edu>
2023-11-01 14:47:46 -0500

    redoing the onClick

--- Frontend/KitchenMingle/app/src/main/java/com/example/as1/IngredientActivity.java
+++ Frontend/KitchenMingle/app/src/main/java/com/example/as1/IngredientActivity.java
@@ -34,32 +34,37 @@ public class IngredientActivity extends AppCompatActivity {
 
         Button PostByBodyBtn = findViewById(R.id.activity_main_post_by_body_button);
         EditText ingredientNameIn = findViewById(R.id.activity_main_recipename_editText);
-
+        quantityEditText = findViewById(R.id.activity_main_quantity_editText);
         tableLayout = findViewById(R.id.tableLayout); // Initialize TableLayout
 
 
         RegenerateAllIngredientsOnScreen(apiText1);
+
         PostByBodyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Ingredient newIngredient = new Ingredient();
-                //Ingredient newQuantity = new Ingredient();
-                String ingredientName = ingredientNameIn.getText().toString();
+                // Get the ingredient name from the EditText field
+                String ingredientName = ingredientNameIn.getText().toString().trim();
+
+                // Get the quantity from the EditText field
                 String quantityString = quantityEditText.getText().toString();
 
-                if (!quantityString.isEmpty()) {
-                    quantity = Integer.parseInt(quantityString);
-                    // Set the quantity directly in the Ingredient object
-                    newIngredient.setQuantity(quantity);
+                if (!ingredientName.isEmpty() && !quantityString.isEmpty()) {
+                    int quantity = Integer.parseInt(quantityString);
+
+                    // Add the row to the table
+                    addDataRow(ingredientName, quantity);
+
+                    // Clear the input fields
+                    ingredientNameIn.setText("");
+                    quantityEditText.setText("");
+
+                    // Create a new Ingredient and send it to the server
+                    Ingredient newIngredient = new Ingredient();
+                    newIngredient.setIngredientName(ingredientName);
 
                     GetIngredientAPI().PostIngredientByBody(newIngredient).enqueue(new SlimCallback<Ingredient>(ingredient -> {
                         RegenerateAllIngredientsOnScreen(apiText1);
-                        ingredientNameIn.setText("");
-                        quantityEditText.setText(""); // Clear the quantity field
-
-                        if (!ingredientName.isEmpty() && quantity > 0) {
-                            addDataRow(ingredientName, quantity);
-                        }
                     }));
                 }
             }
@@ -105,94 +110,51 @@ public class IngredientActivity extends AppCompatActivity {
             row.addView(quantityTextView);
 
             tableLayout.addView(row, rowNumber++);
+
+
+//                TableRow row = new TableRow(this);
+//                row.setId(rowNumber); // Set a unique ID for the row
+//                TextView ingredientNameTextView = new TextView(this);
+//                ingredientNameTextView.setText(ingredientName);
+//
+//                TextView quantityTextView = new TextView(this);
+//                quantityTextView.setText(String.valueOf(quantity));
+//
+//                row.addView(ingredientNameTextView);
+//                row.addView(quantityTextView);
+//
+//                tableLayout.addView(row);
+//                rowNumber++; // Increment the row number
+//
+//                // Set the text of apiText1 to match the ID of the new row
+//                TextView apiText1 = findViewById(R.id.activity_main_textView1);
+//                apiText1.setText("Row ID: " + row.getId());
+
+
+//            TableRow row = new TableRow(this);
+//
+//            TextView ingredientNameTextView = new TextView(this);
+//            ingredientNameTextView.setText(ingredientName);
+//
+//            TextView quantityTextView = new TextView(this);
+//            quantityTextView.setText(String.valueOf(quantity));
+//
+//            row.addView(ingredientNameTextView);
+//            row.addView(quantityTextView);
+//
+//            tableLayout.addView(row, rowNumber++);
         }
 
 
-    void RegenerateAllIngredientsOnScreen(TextView apiText1){
-        GetIngredientAPI().GetAllIngredients().enqueue(new SlimCallback<List<Ingredient>>(ingredients ->{
+    void RegenerateAllIngredientsOnScreen(TextView apiText1) {
+        GetIngredientAPI().GetAllIngredients().enqueue(new SlimCallback<List<Ingredient>>(ingredients -> {
             apiText1.setText("");
 
-            for(int i = ingredients.size() - 1; i >= 0; i--){
+            for (int i = ingredients.size() - 1; i >= 0; i--) {
                 apiText1.append(ingredients.get(i).printable());
             }
         }, "GetAllIngredients"));
     }
-
-
-//    private void addDataRow(String ingredientName) {
-//        TableRow row = new TableRow(this);
-//
-//        TextView ingredientNameTextView = new TextView(this);
-//        ingredientNameTextView.setText(ingredientName);
-//
-//        TextView quantityTextView = new TextView(this);
-//        quantityTextView.setText("0");
-//
-//        Button increaseButton = new Button(this);
-//        increaseButton.setText("+");
-//        increaseButton.setTag("increase" + rowNumber);
-//        increaseButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                increaseQuantity(view);
-//            }
-//        });
-//
-//        Button decreaseButton = new Button(this);
-//        decreaseButton.setText("-");
-//        decreaseButton.setTag("decrease" + rowNumber);
-//        decreaseButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                decreaseQuantity(view);
-//            }
-//        });
-//
-//        row.addView(ingredientNameTextView);
-//        row.addView(quantityTextView);
-//        row.addView(increaseButton);
-//        row.addView(decreaseButton);
-//
-//        tableLayout.addView(row);
-//        rowNumber++; // Increment the row number
-//    }
-
-
-//    public void decreaseQuantity(View view) {
-//        TableRow row = (TableRow) view.getParent(); // Get the parent row
-//        TextView quantityTextView = row.findViewWithTag("quantityTextView");
-//
-//        // Get the current quantity as a string
-//        String currentQuantityStr = quantityTextView.getText().toString();
-//
-//        try {
-//            int currentQuantity = Integer.parseInt(currentQuantityStr);
-//            if (currentQuantity > 0) {
-//                currentQuantity--; // Decrement the quantity if it's greater than 0
-//                quantityTextView.setText(String.valueOf(currentQuantity)); // Update the TextView
-//            }
-//        } catch (NumberFormatException e) {
-//            // Handle parsing error if the quantity is not a valid integer
-//        }
-//    }
-
-//    public void increaseQuantity(View view) {
-//        TableRow row = (TableRow) view.getParent(); // Get the parent row
-//        TextView quantityTextView = row.findViewWithTag("quantityTextView");
-//
-//        // Get the current quantity as a string
-//        String currentQuantityStr = quantityTextView.getText().toString();
-//
-//        try {
-//            int currentQuantity = Integer.parseInt(currentQuantityStr);
-//            if (currentQuantity > 0) {
-//                currentQuantity++; // Increase the quantity if it's greater than 0
-//                quantityTextView.setText(String.valueOf(currentQuantity)); // Update the TextView
-//            }
-//        } catch (NumberFormatException e) {
-//            // Handle parsing error if the quantity is not a valid integer
-//        }
-//    }
 
 
 }

04ed7e3a506c3da7d52a3868a6f37ecd8dbbce3c
xuanwen <xuanwen@iastate.edu>
2023-11-01 13:24:55 -0500

    Added tentative query methods in CommentRepository

--- Backend/Springboot/src/main/java/RoundTrip/repository/CommentRepository.java
+++ Backend/Springboot/src/main/java/RoundTrip/repository/CommentRepository.java
@@ -1,10 +1,16 @@
 package RoundTrip.repository;
 
 import RoundTrip.model.Comment;
+import RoundTrip.model.Rating;
+import RoundTrip.model.Recipe;
+import RoundTrip.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByUserName(String username);
+    List<Comment> findByUser(Users users);
+    List<Comment> findByRecipe(Recipe recipe);
+    List<Comment> findByUsersAndRecipe(Users user, Recipe recipe);
 }

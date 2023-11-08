d593548617aa4f2a5b41004df273a5e189ae54a3
mettryc <mettryc@iastate.edu>
2023-11-07 19:44:36 -0600

    enabling websocket in the back

--- Backend/Springboot/src/main/java/RoundTrip/SpringbootApplication.java
+++ Backend/Springboot/src/main/java/RoundTrip/SpringbootApplication.java
@@ -2,8 +2,10 @@ package RoundTrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
+@EnableWebSocket
 public class SpringbootApplication {
 
 	public static void main(String[] args) {

7c3585812c703fb83fa25c6ece44ee6ea0880517
xuanwen <xuanwen@iastate.edu>
2023-11-30 22:12:20 -0600

    add query for find recipe by a set of ingredients

--- Backend/Springboot/src/main/java/RoundTrip/repository/RecipeRepository.java
+++ Backend/Springboot/src/main/java/RoundTrip/repository/RecipeRepository.java
@@ -1,11 +1,13 @@
 package RoundTrip.repository;
 
+import RoundTrip.model.Ingredient;
 import RoundTrip.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Set;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
@@ -15,5 +17,8 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     @Query("SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE LOWER(i.ingredientName) LIKE %:ingredientName%")
     List<Recipe> findRecipesByIngredientName(@Param("ingredientName") String ingredientName);
+
+    @Query("SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i IN :ingredients")
+    List<Recipe> findRecipesByIngredientSet(@Param("ingredients") Set<Ingredient> ingredients);
 
 }

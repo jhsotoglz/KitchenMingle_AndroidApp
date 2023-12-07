04be03fda75f766c08fcc325f12bcec5dfbb1d22
mettryc <mettryc@iastate.edu>
2023-12-07 17:24:17 -0600

    updating pantryingredient api to match backend changes

--- Frontend/KitchenMingle/app/src/main/java/com/example/as1/api/PantryIngredientApi.java
+++ Frontend/KitchenMingle/app/src/main/java/com/example/as1/api/PantryIngredientApi.java
@@ -17,78 +17,20 @@ import retrofit2.http.Path;
  * This interface defines the contract for interacting with the Pantry Ingredient API.
  */
 public interface PantryIngredientApi {
-    /**
-     * Retrieves a list of all pantry ingredients.
-     * @return A Call object with a List of PantryIngredient.
-     */
-    @GET("ingredient/all")
-    Call<List<PantryIngredient>> GetAllIngredients();
 
-    /**
-     * Posts a new pantry ingredient with the specified name using a path parameter.
-     * @param name The name of the pantry ingredient to be posted.
-     * @return A Call object with the posted PantryIngredient.
-     */
-    @POST("ingredient/post/{name}")
-    Call<PantryIngredient> PostIngredientByPath(@Path("name") String name);
+    @POST("/pantryIng/add/{userId}/{ingredientId}")
+    Call<Set<PantryIngredient>> addPantryIngredient(@Path("userId") Long userId, @Path("ingredientId") Long ingredientId);
 
-    /**
-     * Posts a new pantry ingredient using the request body.
-     * @param newIngredient The new PantryIngredient to be posted.
-     * @return A Call object with the posted PantryIngredient.
-     */
-    @POST("ingredient/post")
-    Call<PantryIngredient> PostIngredientByBody(@Body PantryIngredient newIngredient);
+    @PUT("/pantryIng/quantity/{userId}/{pantryIngId}/{quantity}")
+    Call<Void> setQuantity(@Path("userId") Long userId, @Path("pantryIngId") Long pantryIngId, @Path("quantity") int quantity);
 
-//    @DELETE("ingredient/delete/{id}")
-//    Call<Void> deleteIngredientById(@Path("id") Long id);
+    @PUT("/pantryIng/increment/{userId}/{pantryIngId}")
+    Call<Void> incrementQuantity(@Path("userId") Long userId, @Path("pantryIngId") Long pantryIngId);
 
-//    @DELETE("pantryIng/delete/{userId}/{pantryIngId}")
-//    Call<Void> deleteIngredientById(@Path("id") Long id);
+    @PUT("/pantryIng/decrement/{userId}/{pantryIngId}")
+    Call<Void> decrementQuantity(@Path("userId") Long userId, @Path("pantryIngId") Long pantryIngId);
 
-    /**
-     * Retrieves a list of pantry ingredients for a specific recipe.
-     * @return A Call object with a List of PantryIngredient for the specified recipe.
-     */
-    @GET("/recipe/{recipeId}/ingredients")
-    Call<List<PantryIngredient>> getIngredientsForRecipe();
-
-    /**
-     * Sets the quantity of a pantry ingredient for a user.
-     * @param userId The ID of the user.
-     * @param pantryIngId The ID of the pantry ingredient.
-     * @param quantity The quantity to be set.
-     * @return
-     */
-    @PUT("pantryIng/quantity/{userId}/{pantryIngId}/{quantity}")
-    Call<PantryIngredient> setQuantity(
-            @Path("userId") Long userId,
-            @Path("pantryIngId") Long pantryIngId,
-            @Path("quantity") int quantity
-    );
-
-    /**
-     * Adds a new pantry ingredient to the user's pantry.
-     * @param userId The ID of the user.
-     * @param ingredientId The ID of the ingredient to be added.
-     * @return A Call object with a Set of PantryIngredient reflecting the updated pantry.
-     */
-    @POST("pantryIng/add/{userId}/{ingredientId}")
-    Call<Set<com.example.as1.model.PantryIngredient>> addPantryIngredient(
-            @Path("userId") Long userId,
-            @Path("ingredientId") Long ingredientId
-    );
-
-    /**
-     * Deletes a pantry ingredient from the user's pantry.
-     * @param userId The ID of the user.
-     * @param pantryIngId The ID of the pantry ingredient to be deleted.
-     * @return A Call object representing the deletion operation.
-     */
-    @DELETE("pantryIng/delete/{userId}/{pantryIngId}")
-    Call<Void> deleteIngredient(
-            @Path("userId") Long userId,
-            @Path("pantryIngId") Long pantryIngId
-    );
-
+    @DELETE("/pantryIng/delete/{userId}/{pantryIngId}")
+    Call<Void> deleteIngredient(@Path("userId") Long userId, @Path("pantryIngId") Long pantryIngId);
 }
+

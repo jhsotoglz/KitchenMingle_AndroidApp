33b1dfcddc31326d128fb8e7edfb095749377e5a
xuanwen <xuanwen@iastate.edu>
2023-12-08 00:17:35 -0600

    fixed admin and editor controller tests

--- Backend/Springboot/src/test/java/RoundTrip/Controller/AdminControllerTest.java
+++ Backend/Springboot/src/test/java/RoundTrip/Controller/AdminControllerTest.java
@@ -11,7 +11,7 @@ import RoundTrip.model.Admin;
 import RoundTrip.model.Users;
 import RoundTrip.repository.AdminRepository;
 import RoundTrip.repository.UsersRepository;
-import org.testng.annotations.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
--- Backend/Springboot/src/test/java/RoundTrip/Controller/EditorControllerTest.java
+++ Backend/Springboot/src/test/java/RoundTrip/Controller/EditorControllerTest.java
@@ -7,26 +7,29 @@ import RoundTrip.repository.EditorRepository;
 import RoundTrip.repository.IngredientRepository;
 import RoundTrip.repository.RecipeRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Optional;
-import static org.hamcrest.Matchers.is;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+
+import static org.mockito.ArgumentMatchers.any; // or hamcrest?
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
-@SpringJUnitConfig
+@RunWith(SpringRunner.class)
 @WebMvcTest(EditorController.class)
 public class EditorControllerTest {
 
@@ -42,58 +45,54 @@ public class EditorControllerTest {
     @MockBean
     private IngredientRepository ingredientRepository;
 
+    private Editor editor;
+    @BeforeEach
+    public void setUp() {
+        editor = new Editor();
+        editor.setId(1L);
+
+        when(editorRepository.findById(1L)).thenReturn(Optional.of(editor));
+    }
+
     // Test creating a recipe via path variables
     @Test
     public void testPostRecipeByPath() throws Exception {
-        // Mock the editor returned from the repository
-        Editor editor = new Editor();
-        editor.setId(1L);
-        when(editorRepository.findById(anyLong())).thenReturn(Optional.of(editor));
+        setUp();
 
-        // Mock the save method to return a recipe
-        Recipe recipe = new Recipe();
-        recipe.setRecipeName("Test Recipe");
-        recipe.setRecipeInstructions("Test Instructions");
-        when(recipeRepository.save(any(Recipe.class))).thenReturn(recipe);
-
-        // Perform the post request to create a new recipe
         mockMvc.perform(post("/recipe/post/{editorId}/{name}/{instructions}", 1L, "Test Recipe", "Test Instructions")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.recipeName").value("Test Recipe"))
-                .andExpect(jsonPath("$.recipeInstructions").value("Test Instructions"))
-                .andDo(print());  // Temporarily print the response for debugging
+                .andExpect(jsonPath("$.recipeInstructions").value("Test Instructions"));
     }
+
 
     @Test
     public void testPostRecipeByBody() throws Exception {
-        // Mock the editor returned from the repository
-        Editor editor = new Editor();
-        editor.setId(1L);
-        when(editorRepository.findById(anyLong())).thenReturn(Optional.of(editor));
-
+        setUp();
         // Create a Recipe object
         Recipe recipe = new Recipe();
         recipe.setRecipeName("Test Recipe");
         recipe.setRecipeInstructions("Test Instructions");
 
+        // Mock the save method to return a recipe
+        when(recipeRepository.save(any(Recipe.class))).thenReturn(recipe);
+
+        Long editorId = editor.getId();
         // Perform the POST request to create a new recipe
-        mockMvc.perform(post("/recipe/post/{editorId}", 1L)
+        mockMvc.perform(post("/recipe/post/{editorId}", editorId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(recipe)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.recipeName").value("Test Recipe")) // Verify recipeName in the response
-                .andExpect(jsonPath("$.recipeInstructions").value("Test Instructions")) // Verify recipeInstructions in the response
-                .andExpect(jsonPath("$.editor.id").value(1)); // Verify editor.id in the response
+                .andExpect(jsonPath("$.recipeName").value("Test Recipe"))
+                .andExpect(jsonPath("$.recipeInstructions").value("Test Instructions"))
+                .andExpect(jsonPath("$.editor.id").value(editorId));
     }
 
     // Test deleting a recipe by ID
     @Test
     public void testDeleteRecipeById() throws Exception {
-        // Mock the editor returned from the repository
-        Editor editor = new Editor();
-        editor.setId(1L);
-        when(editorRepository.findById(anyLong())).thenReturn(Optional.of(editor));
+        setUp();
 
         doNothing().when(recipeRepository).deleteById(anyLong());
 
@@ -123,7 +122,7 @@ public class EditorControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(updatedInfo)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.editor.email", is("updated@example.com")))
+                .andExpect(jsonPath("$.email", is("updated@example.com")))
                 .andDo(print());
     }
 

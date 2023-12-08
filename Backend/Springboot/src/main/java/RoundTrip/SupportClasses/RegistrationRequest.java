76c9d0db9e64206d9929c0aaf505662652c2a42d
jhsoto <jhsoto@iastate.edu>
2023-12-07 19:42:10 -0600

    New implementation of /register for users.

--- Backend/Springboot/src/main/java/RoundTrip/SupportClasses/RegistrationRequest.java
+++ Backend/Springboot/src/main/java/RoundTrip/SupportClasses/RegistrationRequest.java
@@ -6,7 +6,8 @@ public class RegistrationRequest {
     private String password;
     private String username;
 
-    public RegistrationRequest(String userType, String email, String password) {
+    public RegistrationRequest(String userType, String email, String password, String username) {
+        this.username = username;
         this.userType = userType;
         this.email = email;
         this.password = password;
